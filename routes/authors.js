d6dee01ecc8c838fef7d523f9c0a1ade076a0c8e
SebiKape <seb.kape@gmail.com>
2021-01-22 01:01:20 -0300

    Initial Authors Routes

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,21 @@ const Author = require('../models/author')
 const router = express.Router()
 
 // All author route
-router.get('/', (req, res) => {
-  res.render('authors/index')
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== ""){
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', { 
+      authors,
+      searchOptions: req.query
+    })
+  } catch (error) {
+    res.redirect('/')
+  }
+  
 })
 
 // New author route
@@ -13,16 +26,17 @@ router.get('/new', (req, res) => {
 })
 
 // Create new author
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const author = new Author({
     name: req.body.name
   })
-  author.save( (err,newAuthor) => {
-    if (err){
-      
-    }
-  })
-  res.send(req.body.name)
+  try {
+    newAuthor = await author.save()
+    res.redirect('authors')
+  } catch (error) {
+    res.render('authors/new', {errorMessage: "Error creating Author"})
+  }
+  
 })
 
 module.exports = router
